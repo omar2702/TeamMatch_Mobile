6df295d66d59579bf5722ad307c65b3771c010c6
Maro <omaralonso27@gmail.com>
2021-05-02 17:00:00 -0500

    fix: add plugin "kotlin-kapt" and architecture to the api.

---
+++ TeamMatch/app/src/main/java/pe/edu/upc/teammatch/data/remote/interface/PlayerInterface.kt
@@ -0,0 +1,6 @@
+package pe.edu.upc.teammatch.data.remote.`interface`
+
+
+interface PlayerInterface {
+
+}
---
+++ TeamMatch/app/src/main/java/pe/edu/upc/teammatch/ui/search/adapter/player.java
@@ -0,0 +1,4 @@
+package pe.edu.upc.teammatch.ui.search.adapter;
+
+public class player {
+}
